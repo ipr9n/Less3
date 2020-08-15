4d8c2848a00d6e80a51cdc07e91d82db0c60b14e
Евгений <ipr9nka@gmail.com>
2020-08-15 15:05:10 +0300

    Lesson3 ITR

---
+++ Less3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Less3
+{
+    class Program
+    {
+        private static UserGenerator userGenerator;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<String> result = new List<string>();
+            if (args.Length > 0) userGenerator = new UserGenerator(args[0]);
+
+            if (args.Length == 2)
+                result = TwoArgsGenerate(args);
+            else if (args.Length == 3)
+                result = ThreeArgsGenerate(args);
+
+            result.ForEach(Console.WriteLine);
+        }
+
+        private static List<String> TwoArgsGenerate(string[] args) => 
+            (Int32.TryParse(args[1], out int count) && count >= 0) ?
+                userGenerator.GenerateUser(count) 
+            : new List<string>();
+
+        private static List<String> ThreeArgsGenerate(string[] args) => 
+            (Double.TryParse(args[2], out double mistakeCount) && mistakeCount >= 0 
+                && Int32.TryParse(args[1], out int count) && count >= 0)
+                ? userGenerator.GenerateUser(count, mistakeCount)
+                : new List<string>();
+    }
+}
---
+++ Less3/UserGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Bogus;
+
+namespace Less3
+{
+    public class UserGenerator
+    {
+        Random random = new Random();
+        delegate string UserTextGenerator();
+
+        readonly UserTextGenerator _getUserText;
+        private readonly string _phonePattern;
+        private double _mistakeCounter = 0.0d;
+        private static string _symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890";
+        private Mistakes _mistakeType;
+        private readonly Faker _faker;
+
+        public UserGenerator(string locale)
+        {
+            _getUserText = GetRusEngUser;
+
+            switch (locale)
+            {
+                case "ru_RU":
+                    _faker = new Faker("ru");
+                    _phonePattern = "# (###) ###-##-##";
+                    break;
+
+
+                case "en_US":
+                    _faker = new Faker("en_US");
+                    _phonePattern = "+# (###) ###-####";
+                    _symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                    break;
+
+                default:
+                    _faker = new Faker("ru");
+                    _phonePattern = "+375 29 ###-##-##";
+                    _getUserText = GetBelarussianUser;
+                    break;
+            }
+        }
+        private string GetBelarussianUser() => $"{BelarussianData.names[random.Next(BelarussianData.names.Length - 1)]} " +
+                      $"{BelarussianData.lastNames[random.Next(BelarussianData.lastNames.Length - 1)]} " +
+                      $"{BelarussianData.patronymics[random.Next(BelarussianData.patronymics.Length - 1)]};" +
+                      $"{_faker.Address.ZipCode()}," +
+                      $"г.{BelarussianData.cityes[random.Next(BelarussianData.cityes.Length - 1)]}," +
+                      $"вул.{BelarussianData.streets[random.Next(BelarussianData.streets.Length - 1)]} д.{random.Next(1, 100)},кв.{random.Next(1, 100)};" +
+                      $"{_faker.Phone.PhoneNumber(_phonePattern)}";
+
+        private string GetRusEngUser() => ($"{_faker.Name.FullName()};" +
+                            $"{_faker.Address.ZipCode()}," +
+                            $"{_faker.Address.City()}," +
+                            $" {_faker.Address.StreetAddress(true)};" +
+                            $" {_faker.Phone.PhoneNumber(_phonePattern)}");
+
+        public List<String> GenerateUser(int count, double mistakeCount = 0.0d)
+        {
+            List<String> outputList = new List<string>();
+            _mistakeCounter = mistakeCount;
+
+            for (var x = 0; x < count; x++)
+            {
+                var text = _getUserText();
+                MakeMistake(ref text, mistakeCount);
+                outputList.Add(text);
+            }
+
+            return outputList;
+        }
+
+        private void MakeMistake(ref string text, double mistakeCount)
+        {
+            while (_mistakeCounter >= 1)
+            {
+                var randomIndex = random.Next(0, text.Length - 1);
+
+                switch (_mistakeType)
+                {
+                    case Mistakes.Switch:
+                        _mistakeType = Mistakes.AddChar;
+                        var chararray = text.ToCharArray();
+                        Array.Reverse(chararray, randomIndex, 2);
+                        text = new string(chararray);
+                        break;
+
+                    case Mistakes.AddChar:
+                        text = text.Insert(randomIndex, $"{_symbols[random.Next(0, _symbols.Length)]}");
+                        _mistakeType = Mistakes.RemoveChar;
+                        break;
+
+                    case Mistakes.RemoveChar:
+                        text = text.Remove(randomIndex, 1);
+                        _mistakeType = Mistakes.Switch;
+                        break;
+                }
+                _mistakeCounter--;
+            }
+            _mistakeCounter += mistakeCount;
+        }
+
+        private enum Mistakes
+        {
+            AddChar,
+            RemoveChar,
+            Switch
+        }
+    }
+}
